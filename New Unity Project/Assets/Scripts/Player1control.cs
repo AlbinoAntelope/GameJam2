18ddb279e52f3fe86f0e489a15280508fbc3ff94
AlbinoAntelope <daniel.eryavec@sympatico.ca>
2016-01-23 12:02:14 -0500

    blah

--- New Unity Project/Assets/Scripts/Player1control.cs
+++ New Unity Project/Assets/Scripts/Player1control.cs
@@ -7,6 +7,15 @@ public class Player1control : MonoBehaviour {
 	public float tilt;
 	public Boundary boundary;
 
+	public string axisName;
+	public string fireAxisName;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+
+	public float fireRate = 0.5f;
+	private float nextFire = 0.0f;
+
 	[System.Serializable]
 	public class Boundary{
 		public float xMin, xMax;
@@ -21,11 +30,14 @@ public class Player1control : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButton (fireAxisName) && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
 	}
 
 	void FixedUpdate () {
-		float moveHorizontal = Input.GetAxis("H_1");
+		float moveHorizontal = Input.GetAxis(axisName);
 		Vector3 moveVector = new Vector3 (moveHorizontal, 0.0f, 0.0f);
 		Rigidbody rb = GetComponent<Rigidbody>();
 		rb.velocity = moveVector * speed;

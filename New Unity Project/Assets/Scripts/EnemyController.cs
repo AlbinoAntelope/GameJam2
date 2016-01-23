379b150af18290d2e95b572f22e33a211a6d4f1a
StevenDao <nataku235@gmail.com>
2016-01-23 12:15:35 -0500

    Added Initial Enemy Controller

---
+++ New Unity Project/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public float speed = 0.01f;
+	public float direction;
+
+	// Use this for initialization
+	void Start () {
+		// Intially Enemy has no direction
+		direction = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.up * direction * speed);
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		GameObject gameObj = collision.collider.gameObject;
+
+		if (gameObj.CompareTag ("Shot1")) {
+			direction = 1;
+		}
+		if (gameObj.CompareTag ("Shot2")) {
+			direction = -1;
+		}
+	}
+}

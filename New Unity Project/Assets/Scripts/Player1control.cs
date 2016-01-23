9c8c94bde53f435cbb80185038e7fc60acbc3353
AlbinoAntelope <daniel.eryavec@sympatico.ca>
2016-01-23 11:21:30 -0500

    blah

---
+++ New Unity Project/Assets/Scripts/Player1control.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1control : MonoBehaviour {
+	
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	[System.Serializable]
+	public class Boundary{
+		public float xMin, xMax;
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		Vector3 moveVector = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+		Rigidbody rb = GetComponent<Rigidbody>();
+		rb.velocity = moveVector * speed;
+		rb.position = new Vector3
+			(
+				Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+				Mathf.Clamp(rb.position.y, -100, 100),
+				0.0f
+			);
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, moveHorizontal * -tilt);
+	}
+}
